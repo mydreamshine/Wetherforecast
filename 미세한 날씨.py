5e405eb4cd2e4174c8b19acc4e0bd4806f187000
mydreamshine <32826146+mydreamshine@users.noreply.github.com>
2018-05-16 14:55:16 +0900

    SDL2 Lanch 방식지정

--- 미세한 날씨.py
+++ 미세한 날씨.py
@@ -1,4 +1,14 @@
+import platform
+import os
+
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = ".\\SDL2\\x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = ".\\SDL2\\x64"
+
 import Framework
 from pico2d import *
 
 open_canvas(300, 427, 60)
+
+close_canvas()
