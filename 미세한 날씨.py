8855a3af1c91d55cd17c2d645521a09cf520a83d
Myeong Joon <mydreamshine@naver.com>
2018-05-16 18:33:31 +0900

    리소스 제작

--- 미세한 날씨.py
+++ 미세한 날씨.py
@@ -7,8 +7,9 @@ else:
     os.environ["PYSDL2_DLL_PATH"] = ".\\SDL2\\x64"
 
 import Framework
+import main_state
 from pico2d import *
 
 open_canvas(300, 427, 60)
-
+Framework.run(main_state)
 close_canvas()
