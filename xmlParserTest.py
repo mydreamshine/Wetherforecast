3fef9deb66c9bc19fb7a75c007c5c0f8600c42a5
JSH <firedory@naver.com>
2018-05-21 00:04:45 +0900

    XMLParser 및 날씨객체 정의
    현재날씨 API, 중기예보 날씨 API를 활용하여 urlLib를 통해서 XML 파일을 읽어옴.
    읽어온 XML 파일을 dict형태로 변환
    dict원소 중에서 필요한 날씨 정보만 추출.
    
    날씨객체
    주소, 날짜, 시간을 기준으로
    온도, 기상상태, 강우상태를 알려주는 객체정의

--- xmlParserTest.py
+++ xmlParserTest.py
@@ -1,60 +1,10 @@
 # import xml.etree.ElementTree as ET
 import urllib.request
 import urllib.parse
+import xmltodict
 import json
+from datetime import datetime
 
-
-def getDustinformation(Address):
-    url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?"
-    key = "serviceKey=" + "Iq26py4%2BXq0dUHvM7eXZdaHnh3O8fDOQOTVjMw95Kh94fKJqLYh%2FZH%2BaZ7%2FD%2BojU7RJ6ERZXP8M%2BH%2BnL8a4cbQ%3D%3D"
-    numOfRows = "&numOfRows=" + str(1)
-    sidoname = "&sidoName=" + urllib.parse.quote_plus(Address)  # 한글은 유니코드로 바꿔줘야 한다.
-    ver = "&ver=" + "1.3"
-    type = "&_returnType=json"
-
-    api_url = url + key + numOfRows + sidoname + ver + type
-
-    data = urllib.request.urlopen(api_url).read().decode('utf8')
-    data_json = json.loads(data)
-    numOfRows = "&numOfRows=" + str(data_json["totalCount"])
-
-    api_url = url + key + numOfRows + sidoname + ver + type
-
-    data = urllib.request.urlopen(api_url).read().decode('utf8')
-    data_json = json.loads(data)
-
-    print(data_json)
-
-
-getDustinformation("경기")
-
-def getWeatherinformation(address):
-    url = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?"
-    key = "serviceKey=" + "oTDANY4hsh%2FaYtGulZR1hdreS4rwf6pcEU21%2Bk2x7U8uezVVbJ3valCtUCY3jFLjPW8G2cvbjA%2BzeBsbrhm6pQ%3D%3D"
-    numOfRows = "&numOfRows=" + str(1)
-    base_date = "&base_date="+ '20180519'
-    base_time = "&base_time="+ '0600'
-    nx = "&nx=" + "97"
-    ny = "&ny=" + "74"
-    pageNo = "&pageNo=" + "1"
-    type = "&_type=json"
-
-    api_url = url + key + numOfRows + base_date + base_time + nx+ny+pageNo+type
-
-    data = urllib.request.urlopen(api_url).read().decode('utf8')
-
-    data_json = json.loads(data)
-    data_json = data_json["response"]["body"]
-    numOfRows = "&numOfRows=" + str(data_json["totalCount"])
-
-    api_url = url + key + numOfRows + base_date + base_time + nx+ny+pageNo+type
-
-    data = urllib.request.urlopen(api_url).read().decode('utf8')
-    data_json = json.loads(data)
-
-    print(data_json)
-
-getWeatherinformation('부산')
 
 def conversionCoordToName(name):
     AddrDic = dict()
@@ -81,4 +31,110 @@ def conversionCoordToName(name):
     AddrDic["울릉"] = 127, 127
     AddrDic["독도"] = 144, 123
 
-    nx, ny = AddrDic[name]
+    return AddrDic[name]
+
+
+def getDustinformation(Address):
+    url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?"
+    key = "serviceKey=" + "Iq26py4%2BXq0dUHvM7eXZdaHnh3O8fDOQOTVjMw95Kh94fKJqLYh%2FZH%2BaZ7%2FD%2BojU7RJ6ERZXP8M%2BH%2BnL8a4cbQ%3D%3D"
+    numOfRows = "&numOfRows=" + str(1)
+    sidoname = "&sidoName=" + urllib.parse.quote_plus(Address)  # 한글은 유니코드로 바꿔줘야 한다.
+    ver = "&ver=" + "1.3"
+    type = "&_returnType=json"
+
+    api_url = url + key + numOfRows + sidoname + ver + type
+
+    data = urllib.request.urlopen(api_url).read().decode('utf8')
+    data_json = json.loads(data)
+    numOfRows = "&numOfRows=" + str(data_json["totalCount"])
+
+    api_url = url + key + numOfRows + sidoname + ver + type
+
+    data = urllib.request.urlopen(api_url).read().decode('utf8')
+    data_json = json.loads(data)
+
+    #print(data_json)
+
+
+def getWeatherTomorrow(address):#내일 내일 모래 날씨
+    url = "http://www.kma.go.kr/wid/queryDFS.jsp?"
+    nx, ny = conversionCoordToName(address)
+    gridx = "gridx="+ str(nx)
+    gridy = "&gridy=" + str(ny)
+    api_url = url+gridx+gridy
+    data = urllib.request.urlopen(api_url).read().decode('utf8')
+    data_dict = xmltodict.parse(data)
+    data_list = data_dict['wid']['body']['data']
+    print(data_list)
+    return data_list
+
+getWeatherTomorrow('부산')
+
+def getWeatherToday(address): #오늘날씨
+    url = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData?"
+    key = "serviceKey=" + "oTDANY4hsh%2FaYtGulZR1hdreS4rwf6pcEU21%2Bk2x7U8uezVVbJ3valCtUCY3jFLjPW8G2cvbjA%2BzeBsbrhm6pQ%3D%3D"
+    numOfRows = "&numOfRows=" + str(1)
+    nx, ny = conversionCoordToName(address)
+    base_date = "&base_date="+ '20180520'
+    base_time = "&base_time="+ '2000'
+    nx = "&nx=" + str(nx)
+    ny = "&ny=" + str(ny)
+    pageNo = "&pageNo=" + "1"
+    type = "&_type=json"
+
+    api_url = url + key + numOfRows + base_date + base_time + nx+ny+pageNo+type
+
+    data = urllib.request.urlopen(api_url).read().decode('utf8')
+
+    data_json = json.loads(data)
+    data_json = data_json["response"]["body"]
+    numOfRows = "&numOfRows=" + str(data_json["totalCount"])
+
+    api_url = url + key + numOfRows + base_date + base_time + nx+ny+pageNo+type
+
+    data = urllib.request.urlopen(api_url).read().decode('utf8')
+    data_json = json.loads(data)
+    data_list = data_json['response']['body']['items']['item']
+
+    currentHour = datetime.today().hour
+
+    T, S, P = None, None, None
+    for item in data_list:
+        if item['category'] == 'T1H' and str(currentHour) == str(item['fcstTime'])[0:2]:
+            T = item['fcstValue']
+        elif item['category'] == 'SKY' and str(currentHour) == str(item['fcstTime'])[0:2]:
+            S = item['fcstValue']
+        elif item['category'] == 'PTY' and str(currentHour) == str(item['fcstTime'])[0:2]:
+            P = item['fcstValue']
+        if T and S:
+            break
+
+    # print(data_json)
+    return T, S, P
+
+
+class DustInformation:
+    def __init__(self, Address=None):
+        self.address = Address
+        self.hour = None
+        self.DustValue = None
+
+
+class WetherInformation:
+    def __init__(self):
+        self.address = None
+        self.day = None
+        self.hour = None
+        self.temperature = None
+        self.skyState = None
+        self.ptyState = None
+
+    def Update(self, Address, OffsetDay=0, OffsetHour=0):
+        self.address = Address
+        if OffsetDay == 0 and OffsetHour == 0:
+            self.temperature, self.skyState, self.ptyState = getWeatherToday(Address)
+        else:
+            wetherinfo_List = getWeatherTomorrow(Address)
+            for dict in wetherinfo_List:
+                for item in dict.items():
+                    self.temperature, self.skyState, self.ptyState = item['temp'], item['sky'], item['pty']
